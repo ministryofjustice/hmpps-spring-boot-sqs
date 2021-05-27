bd056f47ce809a7461549b096266489c18de9a90
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-05-27 13:53:17 +0100

    DT-1581 Add test for autoconfiguration

---
+++ src/test/kotlin/uk/gov/justice/hmpps/sqs/HmppsSqsConfigurationTest.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.hmpps.sqs
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootApplication
+class HmppsSqsConfigurationApplication {
+    fun main(args: Array<String>) {
+        runApplication<HmppsSqsConfigurationApplication>(*args)
+    }
+}
+
+@SpringBootTest(classes = [HmppsSqsConfigurationApplication::class])
+class HmppsSqsConfigurationTest {
+
+    @Autowired
+    private lateinit var sqsQueueAdminService: SqsQueueAdminService
+
+    @Test
+    fun `the sqs queue admin service is loaded`() {
+        assertThat(sqsQueueAdminService).isNotNull
+    }
+}
