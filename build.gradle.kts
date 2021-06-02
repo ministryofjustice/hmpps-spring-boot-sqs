2f8e0ba8870e2e6fc9e322a52d3e428b303dd4af
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-06-02 09:10:29 +0100

    DT-1581 Add signatory to sign jars
    Note that we are using this method: https://docs.gradle.org/current/userguide/signing_plugin.html#sec:in-memory-keys

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,9 @@ publishing {
 }
 
 signing {
+  val signingKey: String? by project
+  val signingPassword: String? by project
+  useInMemoryPgpKeys(signingKey, signingPassword)
   sign(publishing.publications["maven"])
 }
 
