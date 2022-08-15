b89abb97b78a24c54ecb2b614fa51284eb450634
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-08-15 12:20:07 +0100

    SDI-318 async resource unit tests (#86)
    * SDI-318 Add unit tests for the asynchronous endpoints
    
    * SDI-318 ktlint

--- hmpps-sqs-spring-boot-autoconfigure/build.gradle.kts
+++ hmpps-sqs-spring-boot-autoconfigure/build.gradle.kts
@@ -26,14 +26,16 @@ dependencies {
   implementation("com.google.code.gson:gson:2.10")
   implementation("com.microsoft.azure:applicationinsights-core:3.4.3")
   implementation("org.springframework:spring-jms")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
 
   testImplementation("org.assertj:assertj-core:3.23.1")
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
   testImplementation("org.mockito:mockito-junit-jupiter:4.8.1")
   testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.mockito:mockito-inline:4.8.1")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+  testImplementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 publishing {
