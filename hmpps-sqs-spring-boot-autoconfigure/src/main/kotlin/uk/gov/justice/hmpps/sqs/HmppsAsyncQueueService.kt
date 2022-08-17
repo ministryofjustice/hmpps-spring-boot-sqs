4c125c6e3273ceff6070f1201feaa39c9b0b1663
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-08-17 17:18:19 +0100

    SDI-318 Make the async queue service fully asynchronous (#87)

--- hmpps-sqs-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/sqs/HmppsAsyncQueueService.kt
+++ hmpps-sqs-spring-boot-autoconfigure/src/main/kotlin/uk/gov/justice/hmpps/sqs/HmppsAsyncQueueService.kt
@@ -3,15 +3,18 @@ package uk.gov.justice.hmpps.sqs
 import com.google.gson.GsonBuilder
 import com.google.gson.ToNumberPolicy
 import com.microsoft.applicationinsights.TelemetryClient
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.future.await
 import org.slf4j.LoggerFactory
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest
 import software.amazon.awssdk.services.sqs.model.GetQueueAttributesRequest
-import software.amazon.awssdk.services.sqs.model.Message
 import software.amazon.awssdk.services.sqs.model.QueueAttributeName
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
-import kotlin.coroutines.suspendCoroutine
 import kotlin.math.min
 import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest as AwsPurgeQueueRequest
 
@@ -25,7 +28,6 @@ open class HmppsAsyncQueueService(
   private companion object {
     private val log = LoggerFactory.getLogger(this::class.java)
     private val gson = GsonBuilder().setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).create()
-    private val mapClassType = mapOf<String, Any>().javaClass
   }
 
   private val hmppsAsyncTopics: List<HmppsAsyncTopic> = hmppsAsyncTopicFactory.createHmppsAsyncTopics(hmppsSqsProperties)
@@ -49,44 +51,41 @@ open class HmppsAsyncQueueService(
       .map { retryDlqRequest -> retryDlqMessages(retryDlqRequest) }
 
   private suspend fun HmppsAsyncQueue.retryDlqMessages(): RetryDlqResult {
-    if (sqsAsyncDlqClient == null || dlqUrl == null) return RetryDlqResult(0, mutableListOf())
+    if (sqsAsyncDlqClient == null || dlqUrl == null) return RetryDlqResult(0, listOf())
     val messageCount = sqsAsyncDlqClient.countMessagesOnQueue(dlqUrl!!)
-    val messages = mutableListOf<Message.Builder>()
-    repeat(messageCount) {
-      sqsAsyncDlqClient.receiveMessage(ReceiveMessageRequest.builder().queueUrl(dlqUrl).maxNumberOfMessages(1).attributeNames(QueueAttributeName.ALL).build())
-        .thenApply { it.messages().firstOrNull() }
-        .thenApply { msg ->
-          msg?.run {
-            sqsAsyncClient.sendMessage(SendMessageRequest.builder().queueUrl(queueUrl).messageBody(msg.body()).messageAttributes(msg.messageAttributes()).build())
-            sqsAsyncDlqClient.deleteMessage(DeleteMessageRequest.builder().queueUrl(dlqUrl).receiptHandle(msg.receiptHandle()).build())
-            messages += msg.toBuilder()
-          }
-        }
-    }
+    val map: Map<String, Any> = HashMap()
+
+    val messages = (1..messageCount)
+      .map { sqsAsyncDlqClient.receiveMessage(ReceiveMessageRequest.builder().queueUrl(dlqUrl).maxNumberOfMessages(1).attributeNames(QueueAttributeName.ALL).build()).await() }
+      .mapNotNull { it.messages().firstOrNull() }
+      .map { msg ->
+        sqsAsyncClient.sendMessage(SendMessageRequest.builder().queueUrl(queueUrl).messageBody(msg.body()).messageAttributes(msg.messageAttributes()).build())
+        sqsAsyncDlqClient.deleteMessage(DeleteMessageRequest.builder().queueUrl(dlqUrl).receiptHandle(msg.receiptHandle()).build())
+        DlqMessage(messageId = msg.messageId(), body = gson.fromJson(msg.body(), map.javaClass))
+      }
+
     messageCount.takeIf { it > 0 }
       ?.also { log.info("For dlq ${this.dlqName} we found $messageCount messages, attempted to retry ${messages.size}") }
       ?.also { telemetryClient?.trackEvent("RetryDLQ", mapOf("dlq-name" to dlqName, "messages-found" to "$messageCount", "messages-retried" to "${messages.size}"), null) }
-    return RetryDlqResult(messageCount, messages.toList())
+
+    return RetryDlqResult(messageCount, messages)
   }
 
   private suspend fun HmppsAsyncQueue.getDlqMessages(maxMessages: Int): GetDlqResult {
     if (sqsAsyncDlqClient == null || dlqUrl == null) return GetDlqResult(0, 0, listOf())
 
-    val messages = mutableListOf<DlqMessage>()
     val messageCount = sqsAsyncDlqClient.countMessagesOnQueue(dlqUrl!!)
     val messagesToReturnCount = min(messageCount, maxMessages)
+    val map: Map<String, Any> = HashMap()
 
-    (1..messagesToReturnCount).map {
-      sqsAsyncDlqClient.receiveMessage(ReceiveMessageRequest.builder().queueUrl(dlqUrl).maxNumberOfMessages(1).build())
-        .thenApply { it.messages().firstOrNull() }
-        .thenApply { msg ->
-          msg?.run {
-            messages += DlqMessage(messageId = msg.messageId(), body = gson.fromJson(msg.body(), mapClassType))
-          }
-        }
-    }.toTypedArray()
-    // CompletableFuture.allOf(*messageFutures) TODO do I need this? Or does suspend magic just work and I can switch back to repeat(messagesToReturnCount)?
-    return GetDlqResult(messageCount, messagesToReturnCount, messages.toList())
+    val messages = (1..messagesToReturnCount)
+      .asFlow()
+      .map { sqsAsyncDlqClient.receiveMessage(ReceiveMessageRequest.builder().queueUrl(dlqUrl).maxNumberOfMessages(1).build()).await() }
+      .mapNotNull { it.messages().firstOrNull() }
+      .map { msg -> DlqMessage(messageId = msg.messageId(), body = gson.fromJson(msg.body(), map.javaClass)) }
+      .toList()
+
+    return GetDlqResult(messageCount, messagesToReturnCount, messages)
   }
 
   open suspend fun purgeQueue(request: PurgeAsyncQueueRequest): PurgeQueueResult =
@@ -110,11 +109,11 @@ open class HmppsAsyncQueueService(
 data class RetryAsyncDlqRequest(val hmppsQueue: HmppsAsyncQueue)
 
 data class GetAsyncDlqRequest(val hmppsQueue: HmppsAsyncQueue, val maxMessages: Int)
-
 data class PurgeAsyncQueueRequest(val queueName: String, val sqsClient: SqsAsyncClient, val queueUrl: String)
 
 internal suspend fun SqsAsyncClient.countMessagesOnQueue(queueUrl: String): Int =
-  suspendCoroutine {
-    this.getQueueAttributes(GetQueueAttributesRequest.builder().queueUrl(queueUrl).attributeNames(QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES).build())
-      .thenApply { it.attributes()[QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES]?.toInt() ?: 0 }
-  }
+  this.getQueueAttributes(GetQueueAttributesRequest.builder().queueUrl(queueUrl).attributeNames(QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES).build())
+    .await()
+    .let {
+      it.attributes()[QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES]?.toInt() ?: 0
+    }
