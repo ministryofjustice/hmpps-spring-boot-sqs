427a2c8062ff905aea14f53e9a48f98f0e092e78
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-11-08 15:00:16 +0000

    SDI-441 Upgrades (#93)

--- hmpps-sqs-spring-boot-starter/build.gradle.kts
+++ hmpps-sqs-spring-boot-starter/build.gradle.kts
@@ -1,23 +1,23 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-  kotlin("jvm") version "1.7.10"
+  kotlin("jvm") version "1.7.20"
   id("maven-publish")
   id("signing")
-  id("com.github.ben-manes.versions") version "0.42.0"
+  id("com.github.ben-manes.versions") version "0.43.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 dependencies {
   api(project(":hmpps-sqs-spring-boot-autoconfigure"))
-  api(platform("com.amazonaws:aws-java-sdk-bom:1.12.316"))
+  api(platform("com.amazonaws:aws-java-sdk-bom:1.12.337"))
   api("com.amazonaws:amazon-sqs-java-messaging-lib:1.1.0")
   api("com.amazonaws:aws-java-sdk-sns")
-  api(platform("org.springframework.boot:spring-boot-dependencies:2.7.4"))
+  api(platform("org.springframework.boot:spring-boot-dependencies:2.7.5"))
   api("org.springframework.boot:spring-boot-starter-web")
   api("org.springframework.boot:spring-boot-starter-security")
   api("org.springframework.boot:spring-boot-starter-actuator")
-  api("com.microsoft.azure:applicationinsights-core:3.4.1")
+  api("com.microsoft.azure:applicationinsights-core:3.4.3")
   api("org.springframework:spring-jms")
 }
 
