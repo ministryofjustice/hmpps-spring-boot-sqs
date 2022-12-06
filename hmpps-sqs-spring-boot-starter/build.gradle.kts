937850007680d53edcea6ebba060e9514dbeab2b
petergphillips <peter@greenthistle.co.uk>
2022-12-06 13:40:39 +0000

    SDI-463: ⬆  Upgrade to latest gradle spring boot (#97)

--- hmpps-sqs-spring-boot-starter/build.gradle.kts
+++ hmpps-sqs-spring-boot-starter/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-  kotlin("jvm") version "1.7.20"
+  kotlin("jvm") version "1.7.22"
   id("maven-publish")
   id("signing")
   id("com.github.ben-manes.versions") version "0.43.0"
@@ -13,7 +13,7 @@ dependencies {
   api(platform("com.amazonaws:aws-java-sdk-bom:1.12.337"))
   api("com.amazonaws:amazon-sqs-java-messaging-lib:1.1.0")
   api("com.amazonaws:aws-java-sdk-sns")
-  api(platform("org.springframework.boot:spring-boot-dependencies:2.7.5"))
+  api(platform("org.springframework.boot:spring-boot-dependencies:2.7.6"))
   api("org.springframework.boot:spring-boot-starter-web")
   api("org.springframework.boot:spring-boot-starter-security")
   api("org.springframework.boot:spring-boot-starter-actuator")
