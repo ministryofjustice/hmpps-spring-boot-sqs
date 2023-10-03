5a3bcadfc53e0eb55a93bf3130e88e369ea88467
Yasin Mustafa <yasin.mustafa@gmail.com>
2023-10-03 17:17:16 +0100

    MOP-105 Upgrade packages - use all latest versions (#152)
    * MOP-105 Upgrade packages - use all latest versions
    
    * Increment version of library
    
    * Add release notes and update README
    
    * Upgrade by a minor version rather than a patch

--- test-app-reactive/build.gradle.kts
+++ test-app-reactive/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4"
-  kotlin("plugin.spring") version "1.8.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.5.0"
+  kotlin("plugin.spring") version "1.9.10"
 }
 
 configurations {
@@ -17,20 +17,20 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
 
   implementation(project(":hmpps-sqs-spring-boot-starter"))
 
   testImplementation("org.assertj:assertj-core:3.24.2")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-  testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.5.0")
+  testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
-  testImplementation("org.testcontainers:localstack:1.18.0")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.460") // needed so that Localstack has access to the AWS SDK V1 API
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+  testImplementation("org.testcontainers:localstack:1.19.1")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.561") // needed so that Localstack has access to the AWS SDK V1 API
   testImplementation("com.google.code.gson:gson:2.10.1")
 }
