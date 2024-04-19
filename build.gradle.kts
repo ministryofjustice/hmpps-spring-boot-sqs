cda29390b5e269c3f03c1267e06a53dc3d022bc1
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-19 14:04:09 +0100

    SDIT-1677: 🔒️ Upgrade to latest spring boot etc. (#169)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
   group = "uk.gov.justice.service.hmpps"
-  version = "3.1.2"
+  version = "3.1.3"
 
   repositories {
     mavenCentral()
--- hmpps-sqs-spring-boot-starter/build.gradle.kts
+++ hmpps-sqs-spring-boot-starter/build.gradle.kts
@@ -11,13 +11,13 @@ plugins {
 dependencies {
   api(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.1.1"))
   api(project(":hmpps-sqs-spring-boot-autoconfigure"))
-  api(platform("software.amazon.awssdk:bom:2.25.22"))
+  api(platform("software.amazon.awssdk:bom:2.25.34"))
   api("software.amazon.awssdk:sns")
   api("io.awspring.cloud:spring-cloud-aws-starter") { exclude("io.awspring.cloud", "spring-cloud-aws-autoconfigure")}
   api("io.awspring.cloud:spring-cloud-aws-sns")
   api("io.awspring.cloud:spring-cloud-aws-sqs")
   api("software.amazon.awssdk:sts")
-  api(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
+  api(platform("org.springframework.boot:spring-boot-dependencies:3.2.5"))
   api("org.springframework.boot:spring-boot-starter-web")
   api("org.springframework.boot:spring-boot-starter-security")
   api("org.springframework.boot:spring-boot-starter-actuator")
