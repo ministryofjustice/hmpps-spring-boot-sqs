2900ba5e50f06a79798c6010f8604bcaa3900d14
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-07-24 10:09:45 +0100

    CPR-395 add test-app implementation and tests

--- test-app-reactive/src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagenameasync/integration/IntegrationTestBase.kt
+++ test-app-reactive/src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagenameasync/integration/IntegrationTestBase.kt
@@ -45,6 +45,7 @@ abstract class IntegrationTestBase {
     outboundSqsDlqClientSpy.purgeQueue(PurgeQueueRequest.builder().queueUrl(outboundDlqUrl).build()).get()
     outboundTestSqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(outboundTestQueueUrl).build()).get()
     outboundTestNoDlqSqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(outboundTestNoDlqQueueUrl).build()).get()
+    fifoSqsClient.purgeQueue(PurgeQueueRequest.builder().queueUrl(fifoQueueUrl).build()).get()
   }
 
   fun HmppsSqsProperties.inboundQueueConfig() =
@@ -66,7 +67,7 @@ abstract class IntegrationTestBase {
   private val outboundQueue by lazy { hmppsQueueService.findByQueueId("outboundqueue") ?: throw MissingQueueException("HmppsQueue outboundqueue not found") }
   private val outboundTestQueue by lazy { hmppsQueueService.findByQueueId("outboundtestqueue") ?: throw MissingQueueException("HmppsQueue outboundtestqueue not found") }
   private val inboundTopic by lazy { hmppsQueueService.findByTopicId("inboundtopic") ?: throw MissingQueueException("HmppsTopic inboundtopic not found") }
-  private val fifoTopic by lazy { hmppsQueueService.findByTopicId("fifotopic") ?: throw MissingQueueException("HmppsTopic fifotopix not found") }
+  private val fifoTopic by lazy { hmppsQueueService.findByTopicId("fifotopic") ?: throw MissingQueueException("HmppsTopic fifotopic not found") }
   private val outboundTestNoDlqQueue by lazy { hmppsQueueService.findByQueueId("outboundtestnodlqqueue") ?: throw MissingQueueException("HmppsQueue outboundtestnodlqqueue not found") }
   private val auditQueue by lazy { hmppsQueueService.findByQueueId("audit") ?: throw MissingQueueException("HmppsQueue audit not found") }
   private val fifoQueue by lazy { hmppsQueueService.findByQueueId("fifoqueue") ?: throw MissingQueueException("HmppsQueue fifoqueue not found") }
--- test-app/src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/integration/HmppsEventProcessingTest.kt
+++ test-app/src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/integration/HmppsEventProcessingTest.kt
@@ -17,6 +17,7 @@ import uk.gov.justice.digital.hmpps.hmppstemplatepackagename.service.HmppsEvent
 import uk.gov.justice.digital.hmpps.hmppstemplatepackagename.service.Message
 import uk.gov.justice.hmpps.sqs.countAllMessagesOnQueue
 import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
+import java.util.UUID
 
 class HmppsEventProcessingTest : IntegrationTestBase() {
 
@@ -91,5 +92,28 @@ class HmppsEventProcessingTest : IntegrationTestBase() {
 
     await untilCallTo { inboundSqsDlqClient.countMessagesOnQueue(inboundDlqUrl).get() } matches { it == 1 }
     assertThat(inboundSqsClient.countAllMessagesOnQueue(inboundQueueUrl).get()).isEqualTo(0)
+  }
+
+  @Test
+  fun `event published to fifo topic is received by fifo queue`() = runTest {
+    val event = HmppsEvent("fifo-event-id", "FIFO-EVENT", "some FIFO contents")
+    fifoSnsClient.publish(
+      PublishRequest.builder().topicArn(fifoTopicArn).message(gsonString(event))
+        .messageGroupId(UUID.randomUUID().toString())
+        .messageAttributes(
+          mapOf("eventType" to MessageAttributeValue.builder().dataType("String").stringValue(event.type).build()),
+        ).build(),
+    )
+
+    await untilCallTo { fifoSqsClient.countMessagesOnQueue(fifoQueueUrl).get() } matches { it == 1 }
+
+    val (message) = ReceiveMessageRequest.builder().queueUrl(fifoQueueUrl).build()
+      .let { fifoSqsClient.receiveMessage(it).get().messages()[0].body() }
+      .let { objectMapper.readValue(it, Message::class.java) }
+    val receivedEvent = objectMapper.readValue(message, HmppsEvent::class.java)
+
+    assertThat(receivedEvent.id).isEqualTo("fifo-event-id")
+    assertThat(receivedEvent.type).isEqualTo("FIFO-EVENT")
+    assertThat(receivedEvent.contents).isEqualTo("some FIFO contents")
   }
 }
