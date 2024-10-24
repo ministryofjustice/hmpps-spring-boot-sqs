3a1860ae97163a3714bfc8dff8bfef39a2424141
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-24 16:27:04 +0100

    SDIT-2141: ⬆️ Upgrade to spring boot 3.3.5 for path directory traversal fix (#198)

--- hmpps-sqs-spring-boot-starter/build.gradle.kts
+++ hmpps-sqs-spring-boot-starter/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-  kotlin("jvm") version "2.0.20"
+  kotlin("jvm") version "2.0.21"
   id("maven-publish")
   id("signing")
   id("com.github.ben-manes.versions") version "0.51.0"
@@ -11,17 +11,17 @@ plugins {
 dependencies {
   api(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.2.0"))
   api(project(":hmpps-sqs-spring-boot-autoconfigure"))
-  api(platform("software.amazon.awssdk:bom:2.28.5"))
+  api(platform("software.amazon.awssdk:bom:2.28.29"))
   api("software.amazon.awssdk:sns")
   api("io.awspring.cloud:spring-cloud-aws-starter") { exclude("io.awspring.cloud", "spring-cloud-aws-autoconfigure")}
   api("io.awspring.cloud:spring-cloud-aws-sns")
   api("io.awspring.cloud:spring-cloud-aws-sqs")
   api("software.amazon.awssdk:sts")
-  api(platform("org.springframework.boot:spring-boot-dependencies:3.3.4"))
+  api(platform("org.springframework.boot:spring-boot-dependencies:3.3.5"))
   api("org.springframework.boot:spring-boot-starter-web")
   api("org.springframework.boot:spring-boot-starter-security")
   api("org.springframework.boot:spring-boot-starter-actuator")
-  api("com.microsoft.azure:applicationinsights-core:3.5.4")
+  api("com.microsoft.azure:applicationinsights-core:3.6.1")
 }
 
 publishing {
