dc1e87898654f82079092c9e7aabbeb910630241
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-12-20 13:33:43 +0000

    SDIT-2362: ⬆️ Upgrade to plugin 6.1.1 and spring boot 3.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 allprojects {
   group = "uk.gov.justice.service.hmpps"
-  version = "5.2.1"
+  version = "5.2.2"
   repositories {
     mavenCentral()
   }
--- test-app-reactive/build.gradle.kts
+++ test-app-reactive/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.1"
   kotlin("plugin.spring") version "2.0.21"
 }
 
@@ -22,17 +22,17 @@ dependencies {
 
   implementation(project(":hmpps-sqs-spring-boot-starter"))
 
-  testImplementation("org.assertj:assertj-core:3.26.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+  testImplementation("org.assertj:assertj-core:3.27.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
   testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.9.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.10.0")
   testImplementation("org.testcontainers:localstack:1.20.4")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.778") // needed so that Localstack has access to the AWS SDK V1 API
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.780") // needed so that Localstack has access to the AWS SDK V1 API
   testImplementation("com.google.code.gson:gson:2.11.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("io.opentelemetry:opentelemetry-extension-trace-propagators")
