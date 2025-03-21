c1f01874597859f61e3bf818e7744788847b1c23
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-03-21 09:30:56 +0000

    SDIT-2676: ⬆️ Upgrade spring boot to 3.4.4

--- hmpps-sqs-spring-boot-autoconfigure/build.gradle.kts
+++ hmpps-sqs-spring-boot-autoconfigure/build.gradle.kts
@@ -2,17 +2,17 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "2.1.10"
-  kotlin("plugin.spring") version "2.1.10"
+  kotlin("jvm") version "2.1.20"
+  kotlin("plugin.spring") version "2.1.20"
   id("maven-publish")
   id("signing")
   id("com.adarshr.test-logger") version "4.0.0"
   id("com.github.ben-manes.versions") version "0.52.0"
   id("se.patrikerdes.use-latest-versions") version "0.2.18"
   id("io.spring.dependency-management") version "1.1.7"
-  id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+  id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
   id("org.owasp.dependencycheck") version "8.4.3"
-  id("org.springframework.boot") version "3.4.3"
+  id("org.springframework.boot") version "3.4.4"
 }
 
 dependencyManagement {
@@ -31,7 +31,7 @@ dependencies {
   implementation("io.awspring.cloud:spring-cloud-aws-sns")
   implementation("io.awspring.cloud:spring-cloud-aws-sqs")
   implementation("com.google.code.gson:gson:2.12.1")
-  implementation("com.microsoft.azure:applicationinsights-core:3.7.0")
+  implementation("com.microsoft.azure:applicationinsights-core:3.7.1")
   implementation("io.opentelemetry:opentelemetry-api")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
@@ -39,8 +39,8 @@ dependencies {
   implementation("com.amazonaws:amazon-sqs-java-extended-client-lib:2.1.1")
 
   testImplementation("org.assertj:assertj-core:3.27.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
-  testImplementation("org.mockito:mockito-junit-jupiter:5.15.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.16.1")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
