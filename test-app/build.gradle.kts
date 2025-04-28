bb66b874db18057aedc90bd009d42696ec0397e9
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-04-28 07:55:10 +0100

    SDIT-2733: ⬆️ Upgrade to dps gradle 8.1.0 and spring boot 3.4.5 (#217)

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
   kotlin("plugin.spring") version "2.1.20"
 }
 
@@ -16,22 +16,22 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
   implementation("io.opentelemetry:opentelemetry-extension-kotlin")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
   implementation(project(":hmpps-sqs-spring-boot-starter"))
 
   testImplementation("org.assertj:assertj-core:3.27.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
-  testImplementation("org.mockito:mockito-junit-jupiter:5.16.1")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.17.0")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
-  testImplementation("org.testcontainers:localstack:1.20.6")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
+  testImplementation("org.testcontainers:localstack:1.21.0")
   testImplementation("com.amazonaws:aws-java-sdk-core:1.12.782") // Needed so Localstack has access to the AWS SDK V1 API
-  testImplementation("com.google.code.gson:gson:2.12.1")
+  testImplementation("com.google.code.gson:gson:2.13.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("io.opentelemetry:opentelemetry-extension-trace-propagators")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
