84952e4863870da5b3af28643dcdceb50a01535e
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-06-23 16:27:38 +0100

    SDIT-2879 Upgrade to Spring Boot 3.5.3

--- test-app-reactive/build.gradle.kts
+++ test-app-reactive/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
   kotlin("plugin.spring") version "2.1.21"
 }
 
@@ -18,22 +18,22 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
   implementation("io.opentelemetry:opentelemetry-extension-kotlin")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.8")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.9")
 
   implementation(project(":hmpps-sqs-spring-boot-starter"))
 
   testImplementation("org.assertj:assertj-core:3.27.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.13.1")
   testImplementation("org.mockito:mockito-junit-jupiter:5.18.0")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
-  testImplementation("org.testcontainers:localstack:1.21.0")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.783") // needed so that Localstack has access to the AWS SDK V1 API
-  testImplementation("software.amazon.awssdk:s3:2.31.49")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
+  testImplementation("org.testcontainers:localstack:1.21.2")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.787") // needed so that Localstack has access to the AWS SDK V1 API
+  testImplementation("software.amazon.awssdk:s3:2.31.68")
   testImplementation("com.google.code.gson:gson:2.13.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("io.opentelemetry:opentelemetry-extension-trace-propagators")
