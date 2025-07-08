8b5c02b7f58af54c9aab1787c624c92b45043585
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-07-08 15:43:55 +0100

    SDIT-2935: ⬆️  Upgrade to hmpps plugin 8.3.1 and kotlin 2.2.0

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
-  kotlin("plugin.spring") version "2.1.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.1"
+  kotlin("plugin.spring") version "2.2.0"
 }
 
 configurations {
@@ -21,7 +21,7 @@ dependencies {
   implementation(project(":hmpps-sqs-spring-boot-starter"))
 
   testImplementation("org.assertj:assertj-core:3.27.3")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.13.1")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.13.3")
   testImplementation("org.mockito:mockito-junit-jupiter:5.18.0")
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
@@ -29,7 +29,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("org.testcontainers:localstack:1.21.2")
+  testImplementation("org.testcontainers:localstack:1.21.3")
   testImplementation("com.amazonaws:aws-java-sdk-core:1.12.787") // Needed so Localstack has access to the AWS SDK V1 API
   testImplementation("com.google.code.gson:gson:2.13.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
